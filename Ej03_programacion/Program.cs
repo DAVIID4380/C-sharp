00ed3aa9f33b1d4bb6f3e7c1da6abd7b32df7643
DAVIID4380 <davidbotina4380@gmail.com>
2022-04-06 00:35:08 -0500

    C#

--- Ej03_programacion/Program.cs
+++ Ej03_programacion/Program.cs
@@ -44,7 +44,7 @@ namespace Ej03_programacion
 
             Console.WriteLine("el resultado de sumar {0} + {1} es {2} ", num1, num2, resultado);
 
-            /*8. Hacer un algoritmo que lea el nombre de una persona y número de horas que estudia en la semana.*/
+            /*08. Hacer un algoritmo que lea el nombre de una persona y número de horas que estudia en la semana.*/
             string nombre;
             double hrs_estud;
 
@@ -56,7 +56,7 @@ namespace Ej03_programacion
 
             Console.WriteLine("Usted se llama {0} y estudia {1} horas a la semana" , nombre , hrs_estud);
 
-            /*9. Hacer un algoritmo que lea el nombre de un estudiante, la cantidad de materias perdidas y la cantidad
+            /*09. Hacer un algoritmo que lea el nombre de un estudiante, la cantidad de materias perdidas y la cantidad
             de materias ganadas.*/
             string nombre_estud;
             int mater_perd;
@@ -92,101 +92,241 @@ namespace Ej03_programacion
             
             Console.WriteLine("el area del rectangulo es {0}" , area);
             Console.WriteLine("el perimetro del rectangulo es {0}" , perimetro);
+            
+            /*11. . Hacer un algoritmo que lea dos números enteros A y B y muestre su diferencia.*/
+            
+            int num1 , num2 , diferencia;
 
-            /*11. Hacer un algoritmo que lea dos números enteros A y B y muestre su diferencia.*/
-
-            int num1;
-            int num2;
-            int total;
-
-            Console.WriteLine("Digite un primer numero entero cualquiera");
+            Console.WriteLine("Digite el valor del primer número");
             num1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("digite un segundo numero entero cualquiera");
+            Console.WriteLine("Digite el valor del segundo número");
             num2 = int.Parse(Console.ReadLine());
 
-            total = num1 - num2;
-            
-            Console.WriteLine("La diferencia de {0} - {1} es {2}" , num1 , num2 , total);
-            
+            diferencia = num1 - num2;
+
+            Console.WriteLine("La diferencia entre {0} y {1} es: " , num1 , num2);
+
             /*12. Hacer un algoritmo que lea el nombre de una persona, el valor de la hora trabajada y el número de
             horas que trabajó. Se debe mostrar el nombre y el pago de la persona.*/
 
             string nombre;
-            double valor_hora;
-            double n_horas;
-            double pago;
+            double val_hora , num_horas , pago;
 
-            Console.WriteLine("Cual es su nombre");
+            Console.WriteLine("Ingrese su nombre");
             nombre = Console.ReadLine();
 
-            Console.WriteLine("digite el valor de la hora trabajada");
-            valor_hora = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la hora trabajada");
+            val_hora = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("digite el numero de horas que trabajo");
-            n_horas = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número de horas que trabajó");
+            num_horas = double.Parse(Console.ReadLine());
 
-            pago = valor_hora * n_horas;
-            
-            Console.WriteLine("Su pago es de {0}" , pago);
+            pago = num_horas * val_hora;
+
+            Console.WriteLine("Su nombre es {0} y su pago es de: ${1} " , nombre , pago);
 
             /*13. Pedir el radio de un círculo y calcular su área. A=PI*r^2.*/
 
-            double radio;
-            double area;
+            double radio , area;
 
-            Console.WriteLine("Digite el valor del radio de un circulo");
+            Console.WriteLine("Digite el valor del radio de un circulo para calcular su área");
             radio = double.Parse(Console.ReadLine());
 
             area = Math.PI * radio^2;
 
-            Console.WriteLine("El area del circulo es {0}" , area);
+            Console.WriteLine("El área del circulo es {0} " , area);
 
             /*14. Pedir el radio de una circunferencia y calcular su longitud.*/
 
-            double radio;
-            double longitud;
+            double radio , longitud;
 
-            Console.WriteLine("Digite el valor del radio de un circulo");
+            Console.WriteLine("Digite el valor del radio de un circulo para calcular su longitud");
             radio = double.Parse(Console.ReadLine());
 
             longitud = 2 * Math.PI * radio;
 
-            Console.WriteLine("La longitud del circulo es {0}" , longitud);
+            Console.WriteLine("La longitud del circulo es {0} " , longitud);
 
             /*15. Pedir el lado de un cuadrado, mostrar su área y su perímetro.*/
 
-            double lado;
-            double area;
-            double perimetro;
+            double lado , area , perimetro;
 
-            Console.WriteLine("Digite el valor para el lado de un cuadrado");
+            Console.WriteLine("Digite el valor del lado de un cuadrado");
             lado = double.Parse(Console.ReadLine());
 
             area = lado * lado;
-
-            Console.WriteLine("El area del cuadrado es {0}" , area);
-
             perimetro = lado * 4;
 
-            Console.WriteLine("El perimetro del cuadrado es {0}" , perimetro);
+            Console.WriteLine("El área del cuadrado es {0} y el perímetro es {1}" , area , perimetro);
 
-            /*16. Calcular el área de un rectángulo de lados X e Y.0*/
+            /*16. Calcular el área de un rectángulo de lados X e Y.*/
 
-            double ladoX;
-            double ladoY
-            double area;
+            double ladoX , ladoY , area;
 
-            Console.WriteLine("Digite el valor para el lado X de un rectangulo");
+            Console.WriteLine("Digite el valor del lado X de un rectángulo");
             ladoX = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite el valor para el lado X de un rectangulo");
+            Console.WriteLine("Digite el valor del lado Y de un rectángulo");
             ladoY = double.Parse(Console.ReadLine());
 
-            area = ladoY * ladoX;
+            area = ladoX * ladoY;
 
-            Console.WriteLine("El area del rectangulo es {0}" , area);
+            Console.WriteLine("El área del rectángulo es {0}" , area);
 
+            /*17. Pedir dos números y decir si son iguales o no.*/
+
+            double num1 , num2;
+
+            Console.WriteLine("Digite un número cualquiera");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro número cualquiera");
+            num2 = double.Parse(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("Estos números son iguales");
+            }
+            else
+            {
+                Console.WriteLine("Estos números son diferentes");
+            }
+
+            /*18. Pedir un número e indicar si es positivo o negativo.*/
+
+            double num;
+
+            Console.WriteLine("Digite un número cualquiera");
+            num = double.Parse(Console.ReadLine());
+
+            if (num >= 0)
+            {
+                Console.WriteLine("Este número es positivo");
+            }
+            else 
+            {
+                Console.WriteLine("Este número es negativo");
+            }
+
+            /*19. Pedir dos números y decir si uno es múltiplo del otro.*/
+
+            double num1 , num2 , resultado;
+
+            Console.WriteLine("Digite un numero cualquiera");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro numero cualquiera");
+            num2 = double.Parse(Console.ReadLine());
+
+            resultado = num1 * num2;
+
+            Console.WriteLine("{0} multiplicado {1} es {2}" , num1 , num2 , resultado);
+
+            if (resultado % num1 = 0)
+            {
+                Console.WriteLine("{0} es múltiplo de {1} y {2}" , resultado , num1 , num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} no es múltiplo de {1} y {2}" , resultado , num1 , num2);
+            }
+
+            /*20. Pedir dos números y decir cuál es el mayor.*/
+
+            double num1 , num2;
+
+            Console.WriteLine("Digite un número cualquiera");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro número cualquiera");
+            num2 = double.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("{0} es mayor que {1}" , num1 , num2);
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("{0} y {1} son iguales" , num1 , num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} es mayor que {1}" , num2 , num1);
+            }
+
+            /*21. Pedir dos números y decir cuál es el mayor o si son iguales.*/
+
+            double num1 , num2;
+
+            Console.WriteLine("Digite un número cualquiera");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro número cualquiera");
+            num2 = double.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("{0} es mayor que {1}" , num1 , num2);
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("{0} y {1} son iguales" , num1 , num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} es mayor que {1}" , num2 , num1);
+            }
+
+            /*22. Pedir dos números y mostrarlos ordenados de mayor a menor.*/
+
+            double num1 , num2;
+
+            Console.WriteLine("Digite un número cualquiera");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro número cualquiera");
+            num2 = double.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("El orden de mayor a menor es {0} , {1}" , num1 , num2);
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("{0} y {1} son iguales" , num1 , num2);
+            }
+            else
+            {
+                Console.WriteLine("El orden de mayor a menor es {0} , {1}" , num2 , num1);
+            }
+
+            /*23. Pedir tres números y mostrarlos ordenados de mayor a menor.*/
+
+            double num1 , num2 , num3;
+
+            Console.WriteLine("Digite un número cualquiera");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro número cualquiera");
+            num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro número cualquiera");
+            num3 = double.Parse(Console.ReadLine());
+
+            if (num1 > num2 > num3)
+            {
+                Console.WriteLine("El orden de mayor a menor es {0} , {1} , {2}" , num1 , num2 , num3);
+            }
+            else if (num1 == num2 == num3)
+            {
+                Console.WriteLine("{0} , {1} y {2} son iguales" , num1 , num2 , num3);
+            }
+            else
+            {
+                Console.WriteLine("El orden de mayor a menor es {0} , {1} , {2}" , num3 , num2 , num1);
+            }
+            
             Console.ReadKey();
         }
     }
